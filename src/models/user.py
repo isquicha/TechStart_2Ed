972e24cde28d2282f3ac4c7851973768c296fac3
Andre Vicente <isquicha@gmail.com>
2021-02-20 14:48:30 -0300

    Fix: Add constraint to user model
    Username and password must not be null, and the username must be unique

--- src/models/user.py
+++ src/models/user.py
@@ -3,6 +3,6 @@ from src.extensions.database import db
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(140))
+    username = db.Column(db.String(140), unique=True, nullable=False)
     name = db.Column(db.String(140))
-    password = db.Column(db.String(512))
+    password = db.Column(db.String(512), nullable=False)
