c59211edf1f90664f168674cbbbba732b695409f
Andre Vicente <isquicha@gmail.com>
2021-02-21 14:41:23 -0300

    Feat: Add marketplace api

--- src/blueprints/api/__init__.py
+++ src/blueprints/api/__init__.py
@@ -1,10 +1,11 @@
 from flask import Blueprint
 
-from .routes import user
+from .routes import user, marketplace
 
 bp = Blueprint("api", __name__, url_prefix="/api/v1")
 
 user.init_app(bp)
+marketplace.init_app(bp)
 
 
 def init_app(app):
