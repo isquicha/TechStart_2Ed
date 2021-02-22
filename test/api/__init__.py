bb11d19ee417b1ceece329f4dc2882282d7dbf02
Andre Vicente <isquicha@gmail.com>
2021-02-22 13:22:33 -0300

    Test: Restructure api test folder structure

---
+++ test/api/__init__.py
@@ -0,0 +1,5 @@
+API_URL_PREFIX = "/api/v1"
+HEADERS = {
+    "Content-Type": "application/json",
+    "Accept": "application/json",
+}
---
+++ test/api/test_sellers.py
@@ -0,0 +1,27 @@
+import json
+
+from . import API_URL_PREFIX, HEADERS
+
+URL = API_URL_PREFIX + "/sellers"
+
+
+def test_get_request_returns_an_empty_list(client):
+    res = client.get(f"{URL}/")
+    assert res.status_code == 200
+    assert res.json == []
+
+
+def test_get_request_returns_error_on_non_created(client):
+    res = client.get(f"{URL}/1")
+    assert res.status_code == 400
+    assert res.json == {"ERROR": "Seller does not exists"}
+
+
+def test_post_request_fail_when_parameters_are_not_provided(client):
+    res = client.post(
+        f"{URL}/",
+        headers=HEADERS,
+        data=json.dumps({}),
+    )
+    assert res.status_code == 400
+    assert res.json == {"ERROR": "Field 'fantasy_name' must not be empty"}
